23a886f575317c68f814a05cf8ff14c2d7b45cfa
shaun gilchrist <shaun.gilchrist@venafi.com>
2013-01-14 18:54:38 -0700

    adding example

--- apl.js
+++ apl.js
@@ -2,34 +2,163 @@
 (function() {
 
   CodeMirror.defineMode("apl", function(config, parserConfig) {
-    var builtins, wordRE;
-    wordRE = function(words) {
-      return new RegExp("^(?:" + (words.join("|")) + ")$", "i");
+    var builtInFuncs, builtInOps, isArrow, isComment, isFunction, isNiladic, isOperator, stringEater;
+    builtInOps = {
+      ".": "innerProduct",
+      "\\": "scan",
+      "/": "reduce",
+      "⌿": "reduce1Axis",
+      "⍀": "scan1Axis",
+      "¨": "each",
+      "⍣": "power"
     };
-    builtins = "+ − × ÷ ⋆ ○ ?∈ ⌈ ⌊ ⍴".split(" ");
+    builtInFuncs = {
+      "+": ["conjugate", "add"],
+      "−": ["negate", "subtract"],
+      "×": ["signOf", "multiply"],
+      "÷": ["reciprocal", "divide"],
+      "⌈": ["ceiling", "greaterOf"],
+      "⌊": ["floor", "lesserOf"],
+      "∣": ["absolute", "residue"],
+      "⍳": ["indexGenerate", "indexOf"],
+      "?": ["roll", "deal"],
+      "⋆": ["exponentiate", "toThePowerOf"],
+      "⍟": ["naturalLog", "logToTheBase"],
+      "○": ["piTimes", "circularFuncs"],
+      "!": ["factorial", "binomial"],
+      "⌹": ["matrixInverse", "matrixDivide"],
+      "<": [null, "lessThan"],
+      "≤": [null, "lessThanOrEqual"],
+      "=": [null, "equals"],
+      ">": [null, "greaterThan"],
+      "≥": [null, "greaterThanOrEqual"],
+      "≠": [null, "notEqual"],
+      "≡": ["depth", "match"],
+      "≢": [null, "notMatch"],
+      "∈": ["enlist", "membership"],
+      "⍷": [null, "find"],
+      "∪": ["unique", "union"],
+      "∩": [null, "intersection"],
+      "∼": ["not", "without"],
+      "∨": [null, "or"],
+      "∧": [null, "and"],
+      "⍱": [null, "nor"],
+      "⍲": [null, "nand"],
+      "⍴": ["shapeOf", "reshape"],
+      ",": ["ravel", "catenate"],
+      "⍪": [null, "firstAxisCatenate"],
+      "⌽": ["reverse", "rotate"],
+      "⊖": ["axis1Reverse", "axis1Rotate"],
+      "⍉": ["transpose", null],
+      "↑": ["first", "take"],
+      "↓": [null, "drop"],
+      "⊂": ["enclose", "partitionWithAxis"],
+      "⊃": ["diclose", "pick"],
+      "⌷": [null, "index"],
+      "⍋": ["gradeUp", null],
+      "⍒": ["gradeDown", null],
+      "⊤": ["encode", null],
+      "⊥": ["decode", null],
+      "⍕": ["format", "formatByExample"],
+      "⍎": ["execute", null],
+      "⊣": ["stop", "left"],
+      "⊢": ["pass", "right"]
+    };
+    isOperator = /[\.\/⌿⍀¨⍣]/;
+    isNiladic = /⍬/;
+    isFunction = /[\+−×÷⌈⌊∣⍳\?⋆⍟○!⌹<≤=>≥≠≡≢∈⍷∪∩∼∨∧⍱⍲⍴,⍪⌽⊖⍉↑↓⊂⊃⌷⍋⍒⊤⊥⍕⍎⊣⊢]/;
+    isArrow = /←/;
+    isComment = /[⍝#].*$/;
+    stringEater = function(type) {
+      var prev;
+      prev = false;
+      return function(c) {
+        prev = c;
+        if (c === type) {
+          return prev === "\\";
+        }
+        return true;
+      };
+    };
     return {
       startState: function() {
         return {
-          indentStack: null,
-          indentation: 0,
-          mode: false
+          prev: false,
+          func: false,
+          op: false,
+          string: false,
+          escape: false
         };
       },
       token: function(stream, state) {
-        var style, word;
-        style = state.cur(stream, state);
-        word = stream.current();
-        if (builtins.test(word)) {
-          style = "keyword";
+        var ch, funcName, word;
+        if (stream.eatSpace()) {
+          return null;
         }
-        return style;
-      },
-      indent: function(state) {
-        return state.indentStack.indentation;
+        ch = stream.next();
+        if (ch === '"' || ch === "'") {
+          stream.eatWhile(stringEater(ch));
+          stream.next();
+          state.prev = true;
+          return "string";
+        }
+        if (/[\[{\(]/.test(ch)) {
+          state.prev = false;
+          return null;
+        }
+        if (/[\]}\)]/.test(ch)) {
+          state.prev = true;
+          return null;
+        }
+        if (isNiladic.test(ch)) {
+          state.prev = false;
+          return "niladic";
+        }
+        if (/[¯\d]/.test(ch)) {
+          if (state.func) {
+            state.func = false;
+            state.prev = false;
+          } else {
+            state.prev = true;
+          }
+          stream.eatWhile(/[\w\.]/);
+          return "number";
+        }
+        if (isOperator.test(ch)) {
+          return "operator apl-" + builtInOps[ch];
+        }
+        if (isArrow.test(ch)) {
+          return "apl-arrow";
+        }
+        if (isFunction.test(ch)) {
+          funcName = "apl-";
+          if (builtInFuncs[ch] != null) {
+            if (state.prev) {
+              funcName += builtInFuncs[ch][1];
+            } else {
+              funcName += builtInFuncs[ch][0];
+            }
+          }
+          state.func = true;
+          state.prev = false;
+          return "function " + funcName;
+        }
+        if (isComment.test(ch)) {
+          stream.skipToEnd();
+          return "comment";
+        }
+        if (ch === "∘" && stream.peek() === ".") {
+          stream.next();
+          return "function jot-dot";
+        }
+        stream.eatWhile(/[\w\$_]/);
+        word = stream.current();
+        state.prev = true;
+        return "keyword";
       }
     };
   });
 
-  CodeMirror.defineMIME("text/x-apl", "apl");
+  CodeMirror.defineMIME("text/apl", "apl");
 
 }).call(this);
--- src/apl.coffee
+++ src/apl.coffee
@@ -1,22 +1,156 @@
+#many of these regexes directly cribbed from
+#https://github.com/ngn/vim-apl/blob/master/syntax/apl.vim
 CodeMirror.defineMode "apl", (config, parserConfig) ->
-	wordRE = (words) ->
-		new RegExp "^(?:#{words.join "|"})$", "i"
 
-	builtins = "+ − × ÷ ⋆ ○ ?∈ ⌈ ⌊ ⍴".split " "
+	builtInOps = 
+		".": "innerProduct"
+		"\\": "scan"
+		"/": "reduce"
+		"⌿": "reduce1Axis"
+		"⍀": "scan1Axis"
+		"¨": "each"
+		"⍣": "power"
+
+	builtInFuncs = 
+		#sym  #monadic         #dyadic
+		"+": ["conjugate",     "add"]
+		"−": ["negate",        "subtract"]
+		"×": ["signOf",        "multiply"]
+		"÷": ["reciprocal",    "divide"]
+		"⌈": ["ceiling",       "greaterOf"]
+		"⌊": ["floor",         "lesserOf"]
+		"∣": ["absolute",      "residue"]
+		"⍳": ["indexGenerate", "indexOf"]
+		"?": ["roll",          "deal"]
+		"⋆": ["exponentiate",  "toThePowerOf"]
+		"⍟": ["naturalLog",    "logToTheBase"]
+		"○": ["piTimes",       "circularFuncs"]
+		"!": ["factorial",     "binomial"]
+		"⌹": ["matrixInverse", "matrixDivide"]
+		"<": [null,            "lessThan"]
+		"≤": [null,            "lessThanOrEqual"]
+		"=": [null,            "equals"]
+		">": [null,            "greaterThan"]
+		"≥": [null,            "greaterThanOrEqual"]
+		"≠": [null,            "notEqual"]
+		"≡": ["depth",         "match"]
+		"≢": [null,            "notMatch"]
+		"∈": ["enlist",        "membership"]
+		"⍷": [null,            "find"]
+		"∪": ["unique",        "union"]
+		"∩": [null,            "intersection"]
+		"∼": ["not",           "without"]
+		"∨": [null,            "or"]
+		"∧": [null,            "and"]
+		"⍱": [null,            "nor"]
+		"⍲": [null,            "nand"]
+		"⍴": ["shapeOf",       "reshape"]
+		",": ["ravel",         "catenate"]
+		"⍪": [null,            "firstAxisCatenate"]
+		"⌽": ["reverse",       "rotate"]
+		"⊖": ["axis1Reverse",  "axis1Rotate"]
+		"⍉": ["transpose",     null]
+		"↑": ["first",         "take"]
+		"↓": [null,            "drop"]
+		"⊂": ["enclose",       "partitionWithAxis"]
+		"⊃": ["diclose",       "pick"]
+		"⌷": [null,            "index"]
+		"⍋": ["gradeUp",       null]
+		"⍒": ["gradeDown",     null]
+		"⊤": ["encode",        null]
+		"⊥": ["decode",        null]
+		"⍕": ["format",        "formatByExample"]
+		"⍎": ["execute",       null]
+		"⊣": ["stop",          "left"]
+		"⊢": ["pass",         "right"]
+		
+	isOperator = /[\.\/⌿⍀¨⍣]/
+	isNiladic = /⍬/
+	isFunction = /[\+−×÷⌈⌊∣⍳\?⋆⍟○!⌹<≤=>≥≠≡≢∈⍷∪∩∼∨∧⍱⍲⍴,⍪⌽⊖⍉↑↓⊂⊃⌷⍋⍒⊤⊥⍕⍎⊣⊢]/
+	isArrow = /←/
+	isComment = /[⍝#].*$/
+	
+	stringEater = (type) ->
+		prev = false
+		(c) ->
+			prev = c
+			if c is type
+				return prev is "\\"
+			return true
 
 	startState: ->
-		indentStack: null
-		indentation: 0
-		mode: false
+		prev: false
+		func: false
+		op: false
+		string: false
+		escape: false
 
 	token: (stream, state) ->
-		style = state.cur stream, state
+		if stream.eatSpace()
+			return null
+
+		ch = stream.next()
+
+		if ch in ['"', "'"]
+			stream.eatWhile stringEater ch
+			stream.next()
+			state.prev = true
+			return "string"
+
+		if /[\[{\(]/.test ch
+			state.prev = false
+			return null
+
+		if /[\]}\)]/.test ch
+			state.prev = true
+			return null
+
+		if isNiladic.test ch
+			state.prev = false
+			return "niladic"
+
+		if /[¯\d]/.test ch
+			if state.func
+				state.func = false
+				state.prev = false
+			else
+				state.prev = true
+
+			stream.eatWhile /[\w\.]/
+			return "number"
+
+		if isOperator.test ch
+			return "operator apl-#{builtInOps[ch]}"
+
+		if isArrow.test ch
+			return "apl-arrow"
+
+		if isFunction.test ch
+			funcName = "apl-"
+
+			if builtInFuncs[ch]?
+				if state.prev
+					funcName += builtInFuncs[ch][1]
+				else
+					funcName += builtInFuncs[ch][0]
+				
+			state.func = true
+			state.prev = false
+			return "function #{funcName}"
+
+		if isComment.test ch
+			stream.skipToEnd()
+			return "comment"
+
+		if ch is "∘" and stream.peek() is "."
+			stream.next()
+			return "function jot-dot"
+
+		stream.eatWhile /[\w\$_]/
+
 		word = stream.current()
-		if builtins.test word
-			style = "keyword"
-		style
+		state.prev = true
+		return "keyword"
 
-	indent: (state) ->
-		state.indentStack.indentation
 
-CodeMirror.defineMIME "text/x-apl", "apl"
+CodeMirror.defineMIME "text/apl", "apl"
